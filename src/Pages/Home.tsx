007ab1e7a66414bf6650dce445b244a5000d5558
Forlan Ordoñez <nalrofor@gmail.com>
2025-10-19 15:32:56 -0500

    feat: add exercise two ready

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -15,8 +15,7 @@ const Home = () => {
                 <div className="max-w-md">
                     <h1 className="text-5xl font-bold">Hello there</h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        Welcome, this repo has 20 exercises to practice useState, each one increasing in level.
                     </p>
                     <IButton onClick={handleNavigate}>Start</IButton>
                 </div>
--- src/Pages/SwitchLight.tsx
+++ src/Pages/SwitchLight.tsx
@@ -1,8 +1,30 @@
-
+import { useState } from "react"
+import { BsLightningFill } from "react-icons/bs";
+import IButton from "../components/IButton";
 const SwitchLight = () => {
+
+    const [light, setLight] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setLight(!light)
+    }
+
+    const changeLight = light
+        ? "text-amber-400 animate-pulse drop-shadow-[0_0_12px_rgba(251,191,36,1)]"
+        : "text-gray-400"
+
     return (
         <>
-            <div>SwitchLight</div>
+            <div className="flex min-h-[calc(100vh-64px)] justify-center items-center flex-col gap-5.5">
+                <div className="text-6xl">
+                    <BsLightningFill className={changeLight} />
+                </div>
+                <IButton
+                    id="btn"
+                    onClick={handleClick}>
+                    {light ? 'Turn Off' : 'Turn On'}
+                </IButton>
+            </div>
         </>
     )
 }
